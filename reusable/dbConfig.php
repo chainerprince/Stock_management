e7afd4b42f9bd61f60e6883cf779794a5b0c2681
chainerprince <mutegetsi2000@gmail.com>
2021-06-02 21:32:12 +0200

    dbConfig.php

--- reusable/dbConfig.php
+++ reusable/dbConfig.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd = "kayitare@123";
-// $db = "store";
+$pwd = "";
+ $db = "php_work";
 $connection = mysqli_connect($host,$user,$pwd,$db) or die("Error on Db conn".mysqli_connect_error())
 ?>
